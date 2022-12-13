77c7be1081a1e83a293842d5d615cc6533c6f005
skrowll <lucasdejesus.rocha@hotmail.com>
2022-12-13 16:06:01 -0300

    feat: create functions get_unique_job_types & filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -7,43 +7,23 @@ import csv
 def read(path: str) -> List[Dict]:
     with open(path) as csv_file:
         jobs = csv.DictReader(csv_file)
-        jobs_list = []
-        for job in jobs:
-            jobs_list.append(job)
-        return jobs_list
+        # jobs_list = []
+        # for job in jobs:
+        #     jobs_list.append(job)
+        # return jobs_list
+        return [job for job in jobs]
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    jobs_list = []
+    for job in jobs:
+        jobs_list.append(job['job_type'])
+    return set(jobs_list)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return [
+        job for job in jobs
+        if job['job_type'] == job_type
+    ]
