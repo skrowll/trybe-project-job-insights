11a75873c37f173a2442d4d42105d7b10606e8a7
skrowll <lucasdejesus.rocha@hotmail.com>
2022-12-14 22:00:50 -0300

    test: create test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
+    assert count_ocurrences('data/jobs.csv', 'JAVASCRIPT') == 122
+    assert count_ocurrences('data/jobs.csv', 'xablau') == 0
